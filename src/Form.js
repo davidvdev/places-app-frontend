c7e7b079037aaec4167ab530ff974f8df2c2bfbe
davidvdev <dvsummercamp@gmail.com>
2021-08-16 20:33:33 -0500

    added styling

--- src/Form.js
+++ src/Form.js
@@ -17,24 +17,28 @@ const Form = (props) => {
 
     return (
         <form onSubmit={handleSubmit}>
+            <h4>Name:</h4>
             <input 
                 type="text" 
                 name="name" 
                 value={formData.name}
                 onChange={handleChange}   
             />
+            <h4>Image URL</h4>
             <input 
                 type="text" 
                 name="img" 
                 value={formData.img}
                 onChange={handleChange}   
             />
+            <h4>Description</h4>
             <input 
                 type="text" 
                 name="description" 
                 value={formData.description}
                 onChange={handleChange}   
             />
+            <br/>
             <input type="submit" value={props.label} />
         </form>
     )
